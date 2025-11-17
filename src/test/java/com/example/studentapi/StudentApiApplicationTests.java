74129423bddb7385f41e46860dc6410c30bb40ef
AzouzTarek <azouztarek47@gmail.com>
2025-11-17 13:59:08 +0000

    Jenkinsfile

--- src/test/java/com/example/studentapi/StudentApiApplicationTests.java
+++ src/test/java/com/example/studentapi/StudentApiApplicationTests.java
@@ -2,10 +2,8 @@ package com.example.studentapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class StudentApiApplicationTests {
 
 	@Test
